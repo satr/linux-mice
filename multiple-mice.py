437e1073dd87c0636d4fc2776b70615bec5251b8
satr <ssmtor@gmail.com>
2021-06-27 11:08:35 +0100

    edit

--- multiple-mice.py
+++ multiple-mice.py
@@ -3,38 +3,44 @@
 from threading import Thread
 import os.path
 
+thread0 = None
+thread1 = None
+thread2 = None
+thread3 = None
+
 def run():
-    thread0 = None
-    thread1 = None
-    thread2 = None
-    if os.path.exists('/dev/input/mouse0'):
+    global thread0, thread1, thread2, thread3
+    if thread0 is None and os.path.exists('/dev/input/mouse0'):
         print('exists 0')
         thread0 = Thread(target = mouse, args = ('/dev/input/mouse0', "m0"))
         thread0.start()
-    if os.path.exists('/dev/input/mouse1'):
+    if thread1 is None and os.path.exists('/dev/input/mouse1'):
         print('exists 1')
-        thread0 = Thread(target = mouse, args = ('/dev/input/mouse1', "m1"))
-        thread0.start()
-    if os.path.exists('/dev/input/mouse2'):
+        thread1 = Thread(target = mouse, args = ('/dev/input/mouse1', "m1"))
+        thread1.start()
+    if thread2 is None and os.path.exists('/dev/input/mouse2'):
         print('exists 2')
         thread2 = Thread(target = mouse, args = ('/dev/input/mouse2', "m2"))
         thread2.start()
-    if thread0 is not None:
-        thread0.join()
-    if thread1 is not None:
-        thread1.join()
-    if thread2 is not None:
-        thread2.join()
+    if thread3 is None and os.path.exists('/dev/input/mouse3'):
+        print('exists 3')
+        thread3 = Thread(target = mouse, args = ('/dev/input/mouse3', "m3"))
+        thread3.start()
     print("threads finished...exiting")
 
 def mouse(path, name):
     print(path, name)
     mouse = open(path, 'rb')
     while True:
-        status, dx, dy = tuple(c for c in (mouse.read(3)))
-        print("%s: %#02x %d %d" % (name, status, dx, dy))
+        try:
+            status, dx, dy = tuple(c for c in (mouse.read(3)))
+            print("%s: %#02x %d %d" % (name, status, dx, dy))
+        except:
+            print("turned off %s", name)
+            return
 
 
 if __name__ == '__main__':
-    run()
+    Thread(target = run).start()
+    a = input('ok')
 
