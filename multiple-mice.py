a8c151b07c6c0c1070cc80ee793a2c8ed4726add
satr <ssmtor@gmail.com>
2021-06-27 15:49:38 +0100

    edit

--- multiple-mice.py
+++ multiple-mice.py
@@ -10,36 +10,41 @@ thread3 = None
 
 def run():
     global thread0, thread1, thread2, thread3
-    if thread0 is None and os.path.exists('/dev/input/mouse0'):
-        print('exists 0')
-        thread0 = Thread(target = mouse, args = ('/dev/input/mouse0', "m0"))
-        thread0.start()
-    if thread1 is None and os.path.exists('/dev/input/mouse1'):
-        print('exists 1')
-        thread1 = Thread(target = mouse, args = ('/dev/input/mouse1', "m1"))
-        thread1.start()
-    if thread2 is None and os.path.exists('/dev/input/mouse2'):
-        print('exists 2')
-        thread2 = Thread(target = mouse, args = ('/dev/input/mouse2', "m2"))
-        thread2.start()
-    if thread3 is None and os.path.exists('/dev/input/mouse3'):
-        print('exists 3')
-        thread3 = Thread(target = mouse, args = ('/dev/input/mouse3', "m3"))
-        thread3.start()
+    if thread0 is None:
+        thread0 = run_one('/dev/input/mouse0', "m0")
+    if thread1 is None:
+        thread1 = run_one('/dev/input/mouse1', "m1")
+    if thread2 is None:
+        thread2 = run_one('/dev/input/mouse2', "m2")
+    if thread3 is None:
+        thread3 = run_one('/dev/input/mouse3', "m3")
 
-def mouse(path, name):
+def run_one(path, name):
+    if os.path.exists(path):
+        print('exists %s' % name)
+        thread = Thread(target = read_mouse, args = (path, name))
+        thread.start()
+        return thread
+    
+
+def read_mouse(path, name):
     print(path, name)
-    mouse = open(path, 'rb')
+    mouse_file = open(path, 'rb')
     while True:
         try:
-            status, dx, dy = tuple(c for c in (mouse.read(3)))
+            status, dx, dy = tuple(c for c in (mouse_file.read(3)))
+            #next line consumes a lot of CPU, because it prints number of events
             print("%s: %#02x %d %d" % (name, status, dx, dy))
         except:
-            print("turned off %s", name)
+            print("turned off %s" % name)
             return
 
 
 if __name__ == '__main__':
     Thread(target = run).start()
-    a = input('ok')
+    try:
+        a = input('')
+    except:
+        print("stop")
+        exit
 
